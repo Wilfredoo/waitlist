306ec19c52a4e5e267e32d1240179ce4e3eaa6a8
Wilfredo <casas.farach@gmail.com>
2019-04-24 18:25:19 +0200

    ready

--- src/thankyou.js
+++ src/thankyou.js
@@ -11,19 +11,21 @@ export class Thankyou extends React.Component {
 
   render() {
     return (
-      <div className="modal thankyou">
+      <div className="modal">
       <button onClick={this.props.close} className="btn">
         <i class="fa fa-close"></i>
       </button>
  <br />
  <br />
-      <p>We are pretty much peniless and can't pay for ads (for real)</p>
+ <div id="thankyou">
+ <p>      Coolio, you are in. Please be patient now, we'll let you know when it's ready</p>
+      <p>changing topics, we are pretty much peniless and can't pay for ads (for real)</p>
        <p>but if you tell a few folks about this, maybe we can start a chain reaction...</p>
        <br />
       <img id="chainGif" src="https://media.giphy.com/media/3o6gE57nEDKeoCxsrK/giphy.gif" />
       <p> Aren't chain reactions cool?</p>
       <button id="joinButton2" onClick={this.props.close}  type="submit">Yes, they are</button>
-
+</div>
       </div>
     );
   }
--- waitlist.sql
+++ waitlist.sql
@@ -31,37 +31,37 @@ CREATE TABLE poems(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan Carlos', 'Valdelomar', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
-INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan Carlos', 'Valdelomar', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
+-- INSERT INTO waitlist (firstname, lastname, phone, preference, message) VALUES ('Juan', 'Valdivia', '+1304031422332', 'men', 'cool thing you guys');
 
 
 
@@ -93,15 +93,12 @@ INSERT INTO poems (line1, line2, line3, line4) VALUES ('what matters most', 'is 
 INSERT INTO poems (line1, line2) VALUES ('y si te beso', 'salgo ileso?');
 INSERT INTO poems (line1, line2, line3, line4, line5, line6, line7, line8, line9, line10) VALUES ('forgive me if I stumble upon', 'for I know not', 'how to love too well', ' ', 'I am clumsy', 'and my words', 'do not form as I wish', 'so let me kiss you instead', 'and let my lips paint for you', 'all the pictures that my clumsy heart cant''');
 INSERT INTO poems (line1, line2, line3, line4, line5) VALUES ('he''d tease you like that', 'with hungry eyes', 'and licked lips', 'like youre made of', 'nutella');
-INSERT INTO poems (line1, line2, line3, line4, line5, line6, line7, line8, line9) VALUES ('im fighting for this girl', 'on the battlefield of love', 'dont it look like baby cupid', 'sent her arrows from above?', ' ', 'dont you ever leave the side of me', 'indefenitely, not probably', 'and honestly', 'Im down like the economy');
+INSERT INTO poems (line1, line2, line3, line4, line5, line6, line7, line8, line9, line10) VALUES ('im fighting for this girl', 'on the battlefield of love', 'dont it look like baby cupid', 'sent her arrows from above?', ' ', 'dont you ever leave the side of me', 'indefenitely, not probably', 'and honestly', 'Im down like the economy', '- Drake');
 INSERT INTO poems (line1, line2, line3, line4, line5, line6, line7, line8) VALUES ('ow my fire burns', 'hello just throw me', 'into the sea but', 'not to loud', ' ', 'I am sleeping like', 'a little aloe vera plant', 'soothe');
-INSERT INTO poems (line1, line2, line3, line4, line5, line6, line7, line8, line9) VALUES ('no no!', 'please don’t go', 'please stay bae', 'warm inside your bed all day', ' ', 'nuno!', 'please don''t leave', 'please be here', 'just be free and just be real');
 INSERT INTO poems (line1, line2, line3) VALUES ('10:30pm', 'weather’s colder than I thought', 'where is my Uber?');
 INSERT INTO poems (line1, line2, line3, line4) VALUES ('violent winds and', 'silent storms', 'don''t compare to the hurricanes', 'you give me');
 INSERT INTO poems (line1, line2, line3, line4, line5) VALUES ('despite knowing the journey', 'and where it leads', 'I embrace it', 'and I welcome every moment of it', '- Dr Louise Banks, The Arrival');
 INSERT INTO poems (line1, line2, line3, line4, line5, line6) VALUES ('its like every time we spend time', 'it feels like the first time', 'cause we never spend enough time', 'you catch me at the worst time', 'it aint like you not worth time', '- Drake');
-
-
 
 -- INSERT INTO poems (line1, line2, line3, line4, line5, line6) VALUES ('', '', '', '', '', '');
 -- INSERT INTO poems (line1, line2, line3, line4, line5, line6) VALUES ('', '', '', '', '', '');
