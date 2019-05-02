4e387c04cd151571835ba12991a1eb7f92909a79
Wilfredo <casas.farach@gmail.com>
2019-05-02 18:12:10 +0200

    added contact info

--- src/manifiesto.js
+++ src/manifiesto.js
@@ -22,13 +22,13 @@ export class Manifiesto extends React.Component {
 
   render() {
     return (
-      <div className="modal">
+      <div className="modalManifiesto">
         <button onClick={this.props.close} className="btn">
           <i className="fa fa-close" />
         </button>
         <form method="post" onSubmit={() => this.props.submit(this.state)}>
           <div id="manifiesto">
-          <h3>What's wrong with dating apps?</h3>
+          <h3>What's up?</h3>
             <p>
               If you live in Berlin, you have probably seen those ads in the U-Bahn; <span className="adsReference">find love every 11 minutes, single not sorry</span>,
               etc etc... <br /><br /> But we don't really believe in perfect matches or in swiping people like they were merely products of a shopping list.
@@ -42,7 +42,8 @@ export class Manifiesto extends React.Component {
               <br />
               <br />
               <span id="alternative">Colibri will be an alternative dating app.</span> It will be a modest, yet colorful dating app. It won't be shallow, but it'll be fun. It won't be serious, but it'll be real.
-
+              <br /><br />
+            <span id="questions">  *Colibri is brought to you by regular folks. Any questions just send a message to +49 177 7898706 </span>
             </p>
           </div>
         </form>
